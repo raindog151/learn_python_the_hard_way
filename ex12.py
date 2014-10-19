6f4a29a69e115b1c3ec2708bd4c32cd79c07da72
daniel nagy <raindog151@gmail.com>
2014-10-19 19:42:32 +0000

    more exerciseswq

---
+++ ex12.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+age = raw_input("How old are you: ")
+height = raw_input("How tall are you: ")
+weight = raw_input("How heavy are you: ")
+
+print "So you're %r years old, %r inches tall and %r heavy." % (age, height, weight)
