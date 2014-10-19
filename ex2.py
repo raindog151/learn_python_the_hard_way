04f62aee8ce6c7576b84742bc8f162fa44a17aae
dnagy <raindog151@gmail.com>
2014-10-18 20:15:10 -0400

    I am a meat popsicle.

---
+++ ex2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# A comment, this is so you can read your progrma later.
+# Anything after the # is ignored
+
+print "I could have code like this" # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run"
+
+print "But this will"
