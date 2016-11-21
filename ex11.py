d360a27221fb35fa841bfd7997d6188d5a94d8e0
Daniel Nagy <daniel.nagy@target.com>
2016-11-20 20:43:26 -0600

    lpthw 20nov16

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r years old, %r inches tall and weigh %r pounds." % (age,
+                                                                       height,
+                                                                       weight)
